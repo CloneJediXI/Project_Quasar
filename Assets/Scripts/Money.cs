386a6c248eee22eed560fd10b1c46ac3f97c6bb1
Spencer <sburke17@emich.edu>
2020-08-30 21:13:43 -0400

    Tracking, picking up, and saving money

---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    public int value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float speed = Random.Range(3, 17);
+        StartCoroutine(spin(speed));
+    }
+
+    private IEnumerator spin(float speed)
+    {
+        while (true)
+        {
+            transform.Rotate(new Vector3(0, speed, 0));
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInv.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInv : MonoBehaviour
+{
+    public Text text;
+    private int money = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Money"))
+        {
+            money = PlayerPrefs.GetInt("Money");
+        }
+        text.text = money.ToString();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Money")
+        {
+            money += other.GetComponent<Money>().value;
+            text.text = money.ToString();
+            Destroy(other.gameObject);
+            PlayerPrefs.SetInt("Money", money);
+        }
+    }
+    
+}
